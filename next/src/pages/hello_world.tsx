0d11aa20afe4c57ab483e3fab0ef42860f1b806a
Ryunosuke <ttrts@gmail.com>
2024-07-22 12:41:34 +0900

    fix lint

--- next/src/pages/hello_world.tsx
+++ next/src/pages/hello_world.tsx
@@ -1,18 +1,18 @@
-import type { NextPage } from "next";
-import SimpleButton from "@/components/SimpleButton";
+import type { NextPage } from 'next'
+import SimpleButton from '@/components/SimpleButton'
 
 const HelloWorld: NextPage = () => {
   const handleOnClick = () => {
-    console.log("Clicked from hello_world");
-  };
+    console.log('Clicked from hello_world')
+  }
 
   return (
     <>
       <h1>Title</h1>
       <p>content</p>
-      <SimpleButton text={"click"} onClick={handleOnClick} />
+      <SimpleButton text={'click'} onClick={handleOnClick} />
     </>
-  );
-};
+  )
+}
 
-export default HelloWorld;
+export default HelloWorld
